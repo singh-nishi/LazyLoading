043662888c3cfafc521d1df526ac74a3cfb58217
sukanta2017 <sukanta.biswas@rancelab.com>
2017-10-07 14:14:02 +0530

    comment

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { appRouter } from './app.router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    appRouter
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.router.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+export const router: Routes = [
+    {
+        path: '',
+        loadChildren: 'app/components/home/home.module#HomeModule'
+    },
+    {
+        path: 'gallery',
+        loadChildren: 'app/components/gallery/gallery.module#GalleryModule'
+    },
+    {
+        path: 'about',
+        loadChildren: 'app/components/about/about.module#AboutModule'
+    }
+];
+
+export const appRouter: ModuleWithProviders = RouterModule.forRoot(router);
